9c3ffc6c1f0c73c930ecafd23ce439d54219eb56
Jelai Wang <jelaiw@uab.edu>
2018-01-12 09:40:44 -0600

    Update to call fork-new-project.py in production.
    Clean up and document location variables to improve readability.

--- fork-new-project.sh
+++ fork-new-project.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
+# Relative locations of QWRAP support scripts and python-gitlab venv (for now).
 QWRAP_SUPPORT_SCRIPTS_HOME=/share/apps/ngs-ccts/QWRAP-support-scripts
+PYTHON_GITLAB_VENV_HOME=$QWRAP_SUPPORT_SCRIPTS_HOME/python-gitlab-venv
 
 # Load the specific Python3 version we have been testing.
 module load Python/3.6.3-intel-2017a
 
 # Load the python-gitlab venv.
-. $QWRAP_SUPPORT_SCRIPTS_HOME/python-gitlab-venv/bin/activate
+. $PYTHON_GITLAB_VENV_HOME/bin/activate
 
-# Run fork-new-project.py.
-python3 fork-new-project.py "$@"
+# Run fork-new-project.py. Note production symlink.
+python3 $QWRAP_SUPPORT_SCRIPTS_HOME/production/fork-new-project.py "$@"
 
 # Clean up. Deactivate python-gitlab venv and unload Python3 module.
 deactivate
