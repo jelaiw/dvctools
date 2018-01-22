54e8d2400b8dc9bec7fc596ae78f6495970293c2
Jelai Wang <jelaiw@uab.edu>
2018-01-22 09:54:11 -0600

    Add first pass at unit test for fork_new_project.
    Still figuring out how to do mocking in Python. See
    ccts-bmi-incubator#34 for further detail.

---
+++ dvclib_test.py
@@ -0,0 +1,12 @@
+import dvclib
+import unittest
+from unittest.mock import MagicMock
+import gitlab
+
+class DvcLibTests(unittest.TestCase):
+
+	def test_fork_new_project_raises_assert_error_on_wrong_gitlab_api_version(self):
+		mock_gl = MagicMock(spec=gitlab.Gitlab, api_version='3')
+
+		with self.assertRaises(AssertionError):
+			dvclib.fork_new_project(mock_gl, "foo", "bar")
