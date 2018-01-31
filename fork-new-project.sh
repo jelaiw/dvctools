c80a285f5b31a9f1582dae503580d411e5307b60
Jelai Wang <jelaiw@uab.edu>
2018-01-31 10:04:11 -0600

    Update relative location of python-gitlab venv.
    Update variable naming to reflect move to gitlab-api-support-scripts.

--- fork-new-project.sh
+++ fork-new-project.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-# Relative locations of QWRAP support scripts and python-gitlab venv (for now).
-QWRAP_SUPPORT_SCRIPTS_HOME=/share/apps/ngs-ccts/QWRAP-support-scripts
-PYTHON_GITLAB_VENV_HOME=$QWRAP_SUPPORT_SCRIPTS_HOME/python-gitlab-venv
+# Relative locations of GitLab API support scripts and python-gitlab venv.
+GITLAB_API_SUPPORT_SCRIPTS_HOME=/share/apps/ngs-ccts/gitlab-api-support-scripts
+PYTHON_GITLAB_VENV_HOME=$GITLAB_API_SUPPORT_SCRIPTS_HOME/python-gitlab-venv
 
 # Load the specific Python3 version we have been testing.
 module load Python/3.6.3-intel-2017a
@@ -11,7 +11,7 @@ module load Python/3.6.3-intel-2017a
 . $PYTHON_GITLAB_VENV_HOME/bin/activate
 
 # Run fork-new-project.py. Note production symlink.
-python3 $QWRAP_SUPPORT_SCRIPTS_HOME/production/fork-new-project.py "$@"
+python3 $GITLAB_API_SUPPORT_SCRIPTS_HOME/production/fork-new-project.py "$@"
 
 # Clean up. Deactivate python-gitlab venv and unload Python3 module.
 deactivate
