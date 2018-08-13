65102266dccea87cc391df3ad8862487f207ed62
Jelai Wang <jelaiw@uab.edu>
2018-08-13 09:05:19 -0500

    Update help text to improve readability.
    Previous help text was written in terms of "client name", which we have
    dropped in favor of "group path", which is arguably more generally
    applicable.
    
    Perhaps more importantly, updated the example such that it actually works
    for the current version of GitLab (version 10.8.3) API.
    
    See https://gitlab.rc.uab.edu/jelaiw/ccts-bmi-incubator/issues/68.

--- fork-new-project.py
+++ fork-new-project.py
@@ -3,25 +3,25 @@ import gitlab
 from dvclib import fork_new_project
 
 # Set up argument parser.
-parser = argparse.ArgumentParser(description="Fork new GitLab project from a template to given client namespace.")
+parser = argparse.ArgumentParser(description="Fork new GitLab project from a template to given namespace.")
 parser.add_argument(
 	"PROJECT_NAME", help="Name for newly forked GitLab project.")
 parser.add_argument(
-	"CLIENT_NAME", help="Name of client. Example: Bej-Asim")
+	"GROUP_PATH", help="Path to desired GitLab Group namespace. Example: CCTS-Microbiome/Bej-Asim")
 parser.add_argument(
 	"TEMPLATE_PID", help="GitLab project ID of template. Example: 220")
 args = parser.parse_args()
 
 # Log debug so caller can confirm input parameters.
 print("PROJECT_NAME = %s" % args.PROJECT_NAME)
-print("CLIENT_NAME = %s" % args.CLIENT_NAME)
+print("GROUP_PATH = %s" % args.GROUP_PATH)
 print("TEMPLATE_PID = %s" % args.TEMPLATE_PID)
 
 # Create GitLab server connection from config profile.
 gl = gitlab.Gitlab.from_config('uab')
 
 # dvclib.fork_new_project() does the heavy lifting.
-forked_project = fork_new_project(gl, args.PROJECT_NAME, args.CLIENT_NAME, args.TEMPLATE_PID)
+forked_project = fork_new_project(gl, args.PROJECT_NAME, args.GROUP_PATH, args.TEMPLATE_PID)
 
 # Print SSH URL to new repo.
 # You would otherwise have to go to the repo browser to get this.
