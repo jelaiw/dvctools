49392b44e81287614a9dbae1f04ea430b17eaf89
Jelai Wang <jelaiw@uab.edu>
2018-08-29 10:07:07 -0500

    Update with GitLab namespace path for git clone.
    This preserves the GitLab namespace in the DVC backups directory
    structure.

--- backup-repo.py
+++ backup-repo.py
@@ -1,4 +1,14 @@
 import subprocess
+import re
+import pathlib
+
+# Parse a git repo URL for GitLab namespace path.
+def parse_path(git_repo_url):
+	# See https://gitlab.rc.uab.edu/jelaiw/ccts-bmi-incubator/issues/81#note_8797 for implementation decision-making.
+	# See https://docs.python.org/3/howto/regex.html for a refresher.
+	pattern = re.compile('git@gitlab.rc.uab.edu:(\S+).git')
+	match = pattern.match(git_repo_url)
+	return match.group(1)
 
 # Read list of git repos (to back up) from input file.
 # See https://docs.python.org/3.6/library/io.html#i-o-base-classes.
@@ -9,7 +19,15 @@ with open('repo-list.txt') as f:
 repos = [line.rstrip() for line in lines]
 
 # Back up each git repo.
-for repo in repos:
+for git_repo_url in repos:
+	# Parse a git repo URL for GitLab namespace path, which we attempt to preserve in the DVC backups directory structure.
+	path = parse_path(git_repo_url)
+
+	# Make sure this path exists on filesystem.
+	# See https://docs.python.org/3/library/pathlib.html.
+	# I liked pathlib the best, but see https://stackoverflow.com/questions/273192/how-can-i-safely-create-a-nested-directory-in-python for other options.
+	pathlib.Path(path).mkdir(parents=True, exist_ok=True)
+
 	# See https://docs.python.org/3.6/library/subprocess.html#using-the-subprocess-module.
-	# Clone git repo.
-	subprocess.run(['singularity', 'exec', '--bind', '/data', '/share/apps/ngs-ccts/simg/dvctools-0.3.simg', 'git', 'lfs', 'clone', repo], check=False)
+	# Clone git repo to target directory.
+	subprocess.run(['singularity', 'exec', '--bind', '/data', '/share/apps/ngs-ccts/simg/dvctools-0.3.simg', 'git', 'lfs', 'clone', git_repo_url, path], check=False)
