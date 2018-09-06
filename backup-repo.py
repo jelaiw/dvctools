17eb1a8e04af01e51e7df24abec1edd2675996e1
Jelai Wang <jelaiw@uab.edu>
2018-09-06 09:53:26 -0500

    Refactor git-related functions to its own module.
    These include git command line calls (e.g. to git fsck) as well as
    helper functions to pick apart a git repo URL for specific bits.

--- backup-repo.py
+++ backup-repo.py
@@ -1,44 +1,9 @@
 import subprocess
-import re
 import pathlib
 import os
 from shutil import rmtree
 
-# Parse a git repo URL for GitLab namespace as a path.
-def parse_path(git_repo_url):
-	# See https://gitlab.rc.uab.edu/jelaiw/ccts-bmi-incubator/issues/81#note_8797 for implementation decision-making.
-	# See https://docs.python.org/3/howto/regex.html for a refresher.
-	# git@gitlab.rc.uab.edu:CCTS-Microbiome/Bej-Asim/M140-analysis.git
-	pattern = re.compile('git@gitlab.rc.uab.edu:(\S+).git')
-	match = pattern.match(git_repo_url)
-	return match.group(1)
-
-# Run git fsck in git repo at given path.
-def git_fsck(path_to_repo):
-	cwd = os.getcwd() # Save cwd.
-	os.chdir(path_to_repo)
-	# Run git fsck via dvctools "guaranteed DVC stack".
-	subprocess.run(['singularity', 'exec', '--bind', '/data', '/share/apps/ngs-ccts/simg/dvctools-0.3.simg', 'git', 'fsck'], check=False)
-	# Change working directory back.
-	os.chdir(cwd)
-
-# Run git lfs fsck in git repo at given path.
-def git_lfs_fsck(path_to_repo):
-	cwd = os.getcwd() # Save cwd.
-	os.chdir(path_to_repo)
-	# Run git lfs fsck via dvctools "guaranteed DVC stack".
-	subprocess.run(['singularity', 'exec', '--bind', '/data', '/share/apps/ngs-ccts/simg/dvctools-0.3.simg', 'git', 'lfs', 'fsck'], check=False)
-	# Change working directory back.
-	os.chdir(cwd)
-
-# Return abbreviated commit hash for git repo at given path.
-def get_head_commit_hash(path_to_repo):
-	cwd = os.getcwd() 
-	os.chdir(path_to_repo)
-	# See https://docs.python.org/3.6/library/subprocess.html#frequently-used-arguments regarding universal_newlines.
-	cp = subprocess.run(['singularity', 'exec', '--bind', '/data', '/share/apps/ngs-ccts/simg/dvctools-0.3.simg', 'git', 'log', '-1', '--pretty=format:%h'], check=False, stdout=subprocess.PIPE, universal_newlines=True)
-	os.chdir(cwd)
-	return cp.stdout
+from dvclib.git import parse_path, git_fsck, git_lfs_fsck, get_head_commit_hash
 
 # Back up git repo to a single file, right now, a 7zip archive.
 def backup_repo(path_to_repo):
