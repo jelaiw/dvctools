01470757878f7e0e299c61c507dce17a2bf68855
Jelai Wang <jelaiw@uab.edu>
2018-10-01 11:09:59 -0500

    Add lftp script for copying backups to Box.
    See https://gitlab.rc.uab.edu/jelaiw/ccts-bmi-incubator/issues/84#note_9940.

---
+++ mirror-backups-to-box.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --partition=short
+#SBATCH --job-name=dvc_backup_mirror_test
+#SBATCH --time=12:00:00
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=jelaiw@uab.edu
+
+#DVC_BACKUPS_DIR=/data/scratch/jelaiw/dvc-backups
+
+# Look at netrc for CCTS-Boxacct@uab.edu l/p.
+lftp -e "lcd /data/scratch/jelaiw; mirror -R dvc-backups; bye" ftp.box.com
