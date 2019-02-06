9d052bf77cc84e0f4acd5e3ffb398fb200a8d2c3
Jelai Wang <jelaiw@uab.edu>
2019-02-06 16:07:27 -0600

    Fix mock in unit test.
    I did not really understand how mocks worked in Python, so the earlier
    version didn't actually mock get_remote_head_commit_hash(), it was
    calling the real thing, so it seemed to "work".
    
    Spent a couple hours this afternoon trying to understand how patch()
    worked, finally got it. :-D
    
    The links at
    https://www.toptal.com/python/an-introduction-to-mocking-in-python,
    https://medium.com/@yeraydiazdiaz/what-the-mock-cheatsheet-mocking-in-python-6a71db997832,
    and https://docs.python.org/3/library/unittest.mock.html#where-to-patch
    were helpful.

--- dvclib/backup_test.py
+++ dvclib/backup_test.py
@@ -1,16 +1,15 @@
 import unittest
-from unittest.mock import MagicMock
+from unittest.mock import patch
 
 from dvclib.backup import create_backup_file_name
 
 class BackupTests(unittest.TestCase):
-
-	def test_create_backup_file_name_returns_expected_name_for_Bej_test(self):
-		# See https://docs.python.org/3/library/unittest.mock.html.
-		# This works, but need to revisit and better understand Python mocks.
-		mock = MagicMock()
-		mock.get_remote_head_commit_hash.return_value = '78dea3f7714cde9732def1c4ae566bb383a665d6'
-
+	# See https://docs.python.org/3/library/unittest.mock.html.
+	# Especially, see https://docs.python.org/3/library/unittest.mock.html#where-to-patch.
+	# See https://www.toptal.com/python/an-introduction-to-mocking-in-python.
+	# Maybe, see https://medium.com/@yeraydiazdiaz/what-the-mock-cheatsheet-mocking-in-python-6a71db997832.
+	@patch('dvclib.backup.get_remote_head_commit_hash', return_value='88dea3f7714cde9732def1c4ae566bb383a665d6')
+	def test_create_backup_file_name_returns_expected_name_for_Bej_test(self, mocked_dvclib_backup_get_remote_head_commit_hash):
 		git_repo_url = 'git@gitlab.rc.uab.edu:CCTS-Microbiome/Bej-Asim/M140-analysis.git'
-		expected_name = 'CCTS-Microbiome/Bej-Asim/M140-analysis/M140-analysis-78dea3f.7z'
+		expected_name = 'CCTS-Microbiome/Bej-Asim/M140-analysis/M140-analysis-88dea3f.7z'
 		self.assertEqual(expected_name, create_backup_file_name(git_repo_url))
