e9d32899b04ed6229112cb7f0a8c050a3b7baadf
Jelai Wang <jelaiw@uab.edu>
2019-05-16 13:48:20 -0500

    Improve glob pattern specificity.
    Match *.7z.001 and *.7z only. If we match *.7z* then we get *.7z.002
    files, for example, which don't "7za l" as expected (plus we don't want
    to double count).

--- dvc-report.py
+++ dvc-report.py
@@ -12,18 +12,17 @@ if len(sys.argv) != 2:
 target_dir = sys.argv[1]
 print("Target directory = {}".format(target_dir))
 
-# Assemble glob pattern for matching 7zip archives.
-glob_pattern = os.path.join(target_dir, "**/*.7z*")
-print("Glob pattern = {}".format(glob_pattern))
+# Match paths to multivolume 7zip archives.
+paths = glob("**/*.7z.001", recursive=True)
+# Add paths to "legacy" 7zip archives.
+paths.extend(glob("**/*.7z", recursive=True))
 
-paths = glob(glob_pattern, recursive=True)
-
-# Parse 7za l output for data of interest.
+# Parse 7za list archive output for summary statistics of interest.
 data = list()
 for path in paths:
 	# Call 7za l and save standard output.
 	cp = subprocess.run(['7za', 'l', path], check=False, stdout=subprocess.PIPE, universal_newlines=True)
-	# Parse total size and number of files from 7za list output.
+
 	tot_size, num_files = get_7zip_archive_stats(cp.stdout)
 	data.append( (path, tot_size, num_files) )
 
