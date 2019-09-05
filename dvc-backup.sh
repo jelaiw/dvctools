76769ae26b7761965a9c2fc4c3b9d98e6213a349
Jelai Wang <jelaiw@uab.edu>
2019-09-05 09:12:46 -0500

    Update DVC backup SLURM job with dvctools/1.2.

--- dvc-backup.sh
+++ dvc-backup.sh
@@ -12,7 +12,7 @@
 DVC_BACKUPS_DIR=/data/project/bioitx/dvc-backups
 
 # Load dependencies. 
-module load dvctools/1.1
+module load dvctools/1.2
 
 # Change dir so that relative paths in backup script work. Improve this later.
 cd $DVC_BACKUPS_DIR
