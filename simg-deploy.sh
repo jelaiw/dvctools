5c61904f79d2fa8a5027fd451956b54f834b051d
Jelai Wang <jelaiw@uab.edu>
2020-01-23 10:21:49 -0600

    Update SIMG deploy with SINGULARITY_CACHEDIR.
    See https://uab-rc.slack.com/archives/CL8LHFFD0/p1576095791050800?thread_ts=1575994631.017000 for context. The changes made in Dec in response to Tom's issue reports for RA2 DREAM Challenge affect how we write our deploy script.
    
    Update to Singularity version 3.5.2 for SIMG deploy.

--- simg-deploy.sh
+++ simg-deploy.sh
@@ -3,23 +3,24 @@
 VERSION=$1
 DOCKER_URL=docker://$2
 SIMG_FILENAME=dvctools-${VERSION}.simg
+SIMG_PATH=$SINGULARITY_CACHEDIR/$SIMG_FILENAME
 SIMG_DEPLOY_DIR=/share/apps/ngs-ccts/simg
 
 # Load currently recommended Singularity version.
-module load Singularity/2.6.1-GCC-5.4.0-2.26
+module load Singularity/3.5.2-GCC-5.4.0-2.26
 
 # Build Singularity image.
 singularity pull $DOCKER_URL
 
 # Set permissions to read-only.
-chmod 444 $SIMG_FILENAME 
+chmod 444 $SIMG_PATH 
 
 # Copy Singularity image to desired deploy location. 
 # Note -f will overwrite existing SIMG file.
-cp -f $SIMG_FILENAME $SIMG_DEPLOY_DIR
+cp -f $SIMG_PATH $SIMG_DEPLOY_DIR
 
 # Clean up. Need the -f because perms are 444.
-rm -f $SIMG_FILENAME
+rm -f $SIMG_PATH
 
 # Calculate a checksum so we can tell if anything changes.
 sha256sum $SIMG_DEPLOY_DIR/$SIMG_FILENAME > $SIMG_DEPLOY_DIR/${SIMG_FILENAME}.sha256sum
